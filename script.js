5b3fb0b9e0b8713fc1915076b23ec06afd9e0ef2
zobaer <zobaerislam62@gmail.com>
2023-03-18 15:32:37 +0600

    implement the highsocre

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 // Generate secret number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 
 // Display the message
@@ -35,6 +36,11 @@ document.querySelector('.check').addEventListener (
 
             document.querySelector('body').style.backgroundColor = '#60b347';
             document.querySelector('.number').style.width = '30rem';
+
+            if(score > highscore) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = highscore;
+            }
         }
 
         // When guess is too high
@@ -70,15 +76,12 @@ document.querySelector('.again').addEventListener(
     function() {
         score = 20;
         secretNumber = Math.trunc(Math.random() * 20) + 1;
-        // document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.message').textContent = 'Start guessing...';
         document.querySelector('.score').textContent = score;
         document.querySelector('.number').textContent = '?';
         document.querySelector('.guess').value = '';
 
-
         document.querySelector('body').style.backgroundColor = '#222';
         document.querySelector('.number').style.width = '15rem';
-
     }
 );
